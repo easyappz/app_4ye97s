a6f1b7fd94dd2dfc6e524d40889d06cc5e38d1ce
IbragimKhaniev <newinterny@gmail.com>
2025-07-01 17:17:53 +0300

    Update files

--- react/src/App.js
+++ react/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import ErrorBoundary from './ErrorBoundary';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>iPhone Calculator</h1>
+        </header>
+        <main>
+          <ErrorBoundary>
+            <Routes>
+              <Route path="/" element={<Calculator />} />
+            </Routes>
+          </ErrorBoundary>
+        </main>
+      </div>
+    </Router>
   );
 }
 
